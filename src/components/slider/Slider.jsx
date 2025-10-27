7686ed85dfcf315bcb6c43dbb56bd50dae80e7be
Ivan Lorenzo <elhibar77@hotmail.es>
2025-10-27 11:39:17 +0100

    PFF-21 refact: añadida rotacion

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -49,7 +49,7 @@ const Slider = ({ fetchItems, renderItem, title }) => {
         centeredSlides={true}
         slidesPerView={'3'}
         coverflowEffect={{
-          rotate: 0,
+          rotate: 50,
           stretch: 0,
           depth: 100,
           modifier: 1,
