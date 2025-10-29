ec56192e85ab7ef76eb6c0558df0cadc8314336c
Ivan Lorenzo <elhibar77@hotmail.es>
2025-10-29 12:27:45 +0100

    PFF-21 refact: estilos para el boton de categoria ajustados

--- src/components/common/CategoryButton.jsx
+++ src/components/common/CategoryButton.jsx
@@ -4,7 +4,7 @@ import './CategoryButton.css';
 function CategoryButton({ active, onClick, children }) {
   return (
     <button
-      className={`movies-page__category-btn${active ? ' active' : ''}`}
+      className={`category-btn${active ? ' active' : ''}`}
       onClick={onClick}
       type="button"
     >
